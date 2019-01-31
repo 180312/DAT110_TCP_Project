2128f89a703d6c233410414c7b93c613ff3fef1e
180312 <31240925+180312@users.noreply.github.com>
2019-01-31 12:02:47 +0100

    RPCClient finished

--- dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
@@ -41,23 +41,14 @@ public class RPCClient {
 	}
 
 	public byte[] call(byte[] rpcrequest) {
-
-		byte[] rpcreply;
-
-		/*
-		 * TODO:
-		 * 
-		 * Make a remote call on the RPC server by sending a request message and receive
-		 * a reply message
-		 * 
-		 * rpcrequest is the marshalled rpcrequest from the client-stub rpctreply is the
-		 * rpcreply to be unmarshalled by the client-stub
-		 * 
-		 */
-
-		if (true) {
-			throw new RuntimeException("not yet implemented");
-		}
+		
+		Message requestMessage = new Message(rpcrequest);
+		
+		connection.send(requestMessage);
+		
+		Message receivedMessage = connection.receive();
+		
+		byte[] rpcreply = receivedMessage.getData();
 
 		return rpcreply;
 
