12368b3785b698a941a597ac7f5f8d82bfa8145a
571559 <31239471+andersmh@users.noreply.github.com>
2019-01-31 11:16:00 +0100

    added try-catch to connect()

--- dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
+++ dat110-project1/src/no/hvl/dat110/rpc/RPCClient.java
@@ -6,56 +6,58 @@ public class RPCClient {
 
 	private MessagingClient msgclient;
 	private Connection connection;
-	
+
 	public RPCClient(String server, int port) {
-	
-		msgclient = new MessagingClient(server,port);
+
+		msgclient = new MessagingClient(server, port);
 	}
-	
+
 	public void register(RPCStub remote) {
 		remote.register(this);
 	}
-	
+
 	public void connect() {
-		
-		//connect using the underlying messaging layer connection
-		
-		
-		msgclient.connect();
-		
-		throw new RuntimeException("not yet implemented");
-			
-	}
-	
-	public void disconnect() {
-		
-		
-		// TODO: disconnect/close the underlying messaging connection
-		
-		throw new RuntimeException("not yet implemented");
-		
-	}
-	
-	public byte[] call(byte[] rpcrequest) {
-		
-		byte[] rpcreply;
-		
-		/* TODO: 
-		
-		Make a remote call on the RPC server by sending a request message
-		and receive a reply message
-		
-		rpcrequest is the marshalled rpcrequest from the client-stub
-		rpctreply is the rpcreply to be unmarshalled by the client-stub
-		
-		*/
-		
-		if (true) {
-		  throw new RuntimeException("not yet implemented");
+
+		// connect using the underlying messaging layer connection
+
+		try {
+
+			msgclient.connect();
+		} catch (Exception e) {
+			System.out.println("An error occurd: " + e);
 		}
-		
+
+	}
+
+	public void disconnect() {
+
+		// TODO: disconnect/close the underlying messaging connection
+
+		throw new RuntimeException("not yet implemented");
+
+	}
+
+	public byte[] call(byte[] rpcrequest) {
+
+		byte[] rpcreply;
+
+		/*
+		 * TODO:
+		 * 
+		 * Make a remote call on the RPC server by sending a request message and receive
+		 * a reply message
+		 * 
+		 * rpcrequest is the marshalled rpcrequest from the client-stub rpctreply is the
+		 * rpcreply to be unmarshalled by the client-stub
+		 * 
+		 */
+
+		if (true) {
+			throw new RuntimeException("not yet implemented");
+		}
+
 		return rpcreply;
-		
+
 	}
 
 }
